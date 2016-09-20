c0043a7f3486e076ca7ae3233e8308bfe6ec806d
Dustin <gjf66924011@gmail.com>
2016-09-20 11:43:03 +0800

    create a new project

---
+++ src/main/java/com/maple/pdm/PDMTool.java
@@ -0,0 +1,10 @@
+package com.maple.pdm;
+
+/**
+ * Created by Administrator on 2016/9/20 0020.
+ */
+public class PDMTool {
+    public static void main(String[] args) {
+        System.out.println("pdm tools run");
+    }
+}
